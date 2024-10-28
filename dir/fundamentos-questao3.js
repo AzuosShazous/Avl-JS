b0643ec038403b026c7066e450174b8bc07c19ad
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-10-28 08:22:17 -0300

    Cria arquivo da questao 3 de fundamentos

---
+++ dir/fundamentos-questao3.js
@@ -0,0 +1,9 @@
+// Conversão de Celsius para Fahrenheit ((C × 9/5) + 32)
+let celsius = -9
+let Convertido_para_Fahrenheit = (celsius * 9/5) + 32
+console.log(`${celsius}°C é igual a ${Convertido_para_Fahrenheit.toFixed(2)}°F`)
+
+// Conversão de Fahrenheit para Celsius ((F - 32) x 5/9)
+let fahrenheit = 35
+let Convertido_para_Celsius = (fahrenheit - 32) * 5/9
+console.log(`${fahrenheit}°F é igual a ${Convertido_para_Celsius.toFixed(2)}°C`)
