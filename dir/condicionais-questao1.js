bcff479858929e76af59342eae1ea3009d7e1f96
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-10-28 13:54:00 -0300

    Cria arquivo da questao 1 de condicionais

---
+++ dir/condicionais-questao1.js
@@ -0,0 +1,11 @@
+let numA = 3
+let numB = 2
+
+if (numA > numB){
+    console.log(`O maior número é o ${numA} (numA)`)
+}else if(numA<numB){
+    console.log(`O maior número é o ${numB} (numB)`)
+}
+else{
+    console.log(`Não há nenhum número maior, uma vez que ${numA} (numaA) é igual a ${numB} (numB)`)
+}
