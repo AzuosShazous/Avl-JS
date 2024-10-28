56b556367191de980a30aaf8e2f3d54dcb5a1dfd
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-10-28 08:46:04 -0300

    Cria arquivo da questao 5 de fundamentos

---
+++ dir/fundamentos-questao5.js
@@ -0,0 +1,10 @@
+let num1 = 50
+let num2 = 99
+let num3 = 50
+
+if ((num1 >= 50 && num1 <= 99) && (num2 >= 50 && num2 <= 99) && (num3 >= 50 && num3 <= 99)) {
+    console.log("Todos os números estão entre 50 e 99.")
+}
+else {
+    console.log("Erro: um ou mais números não estão no intervalo entre 50 e 99.")
+}
